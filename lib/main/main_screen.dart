c93e25d731f9e13ed930fdf025652d44417fe8bc
Kotlinovsky <kotlinovsky@gmail.com>
2020-11-19 22:18:09 +0300

    Fixed build.

--- lib/main/main_screen.dart
+++ lib/main/main_screen.dart
@@ -1,5 +1,5 @@
 import 'package:droply/common/navigation.dart';
-import 'file:///C:/Users/Kotlinovsky/droply-mobile/lib/main/nearby/nearby_screen.dart';
+import 'package:droply/main/nearby/nearby_screen.dart';
 import 'package:droply/main/network_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:easy_localization/easy_localization.dart';
