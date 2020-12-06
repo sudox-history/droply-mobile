3358546b17e76c8df5786b2bf0732da6cc6df8c3
Kotlinovsky <kotlinovsky@gmail.com>
2020-12-06 22:33:30 +0300

    [FEATURE] Scanning hint showing/hiding animation

--- lib/presentation/main/nearby/nearby_screen.dart
+++ lib/presentation/main/nearby/nearby_screen.dart
@@ -34,7 +34,8 @@ class NearbyScreenState extends State<NearbyScreen> {
             _listKey.currentState.insertItem(1);
           } else if (previous is NearbyScreenScanningState &&
               !(current is NearbyScreenScanningState)) {
-            _listKey.currentState.removeItem(1, _buildItem);
+            _listKey.currentState
+                .removeItem(1, (_, animation) => _buildItem(1, animation));
           }
 
           return false;
@@ -107,71 +108,3 @@ class NearbyScreenState extends State<NearbyScreen> {
     );
   }
 }
-
-// import 'package:droply/common/constants.dart';
-// import 'package:droply/common/device/device_widget.dart';
-// import 'package:droply/common/ui/other_widgets.dart';
-// import 'package:droply/main/nearby/nearby_screen_state.dart';
-// import 'package:flutter/cupertino.dart';
-// import 'package:flutter/material.dart';
-// import 'package:flutter_mobx/flutter_mobx.dart';
-//
-// class NearbyScreen extends StatelessWidget {
-//   final NearbyScreenState _state = NearbyScreenState();
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: Observer(
-//         builder: (_) {
-//           var children = <Widget>[
-//             SizedBox(height: 20),
-//             _buildScanNearbyOption(),
-//           ];
-//
-//           if (_state.isScanningEnabled) {
-//             children.add(_buildScanningHint());
-//           }
-//
-//           if (_state.deviceStates != null) {
-//             children.addAll(_state.deviceStates.map((state) {
-//               return DeviceWidget((){}, state);
-//             }));
-//           }
-//
-//           return ListView(children: children);
-//         },
-//       ),
-//     );
-//   }
-//
-// Padding _buildScanNearbyOption() {
-//   return Padding(
-//     padding: EdgeInsets.symmetric(horizontal: 16),
-//     child: buildSwitchSetting(
-//       "Scan devices nearby",
-//       "We'll show you devices that also use EasyShare",
-//       _state.isScanningEnabled,
-//       (checked) {
-//         _state.toggleScanning();
-//       },
-//     ),
-//   );
-// }
-//
-// Widget _buildScanningHint() {
-//   return Padding(
-//     padding: EdgeInsets.only(top: 20, bottom: 12.5, left: 16, right: 16),
-//     child: Text(
-//       "Scanning for more devices ...",
-//       textAlign: TextAlign.center,
-//       style: TextStyle(
-//         color: AppColors.hintTextColor,
-//         fontFamily: AppFonts.openSans,
-//         fontWeight: AppFonts.regular,
-//         fontSize: 17,
-//       ),
-//     ),
-//   );
-// }
-// }
